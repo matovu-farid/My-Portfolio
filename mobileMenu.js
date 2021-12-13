4c6fe5d885c1974de89145de82c22e7225180b52
Farid <matovu90@gmail.com>
2021-12-13 22:57:56 +0300

    Add menu styling and section navigation

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,11 +1,18 @@
-let mobileMenu = document.querySelector('.mobile-menu');
-let hamburgerMenu = document.querySelector('.hamburger-menu');
-let close = document.querySelector('.close');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const close = document.querySelector('.close');
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuItems = document.querySelectorAll('.mobile-menu li');
 
-hamburgerMenu.addEventListener('click', (event) => {
-  mobileMenu.setAttribute('style', 'display:block');
-})
-
-close.addEventListener('click', (event) => {
+const closeMenu = () => {
   mobileMenu.setAttribute('style', 'display: none');
-})
+};
+
+hamburgerMenu.addEventListener('click', () => {
+  mobileMenu.setAttribute('style', 'display:flex');
+});
+
+close.addEventListener('click', closeMenu);
+
+Array.from(menuItems).forEach((node) => {
+  node.addEventListener('click', closeMenu);
+});
