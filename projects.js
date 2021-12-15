35a4c1fab7f47ff02f346370ba21cc2b0d40bd69
Farid <matovu90@gmail.com>
2021-12-15 14:50:27 +0300

    Fix: javascript spacing linter errors

--- projects.js
+++ projects.js
@@ -1,171 +1,80 @@
-
-const isDesktop =()=>{
-  return window.matchMedia("(min-width: 993px)").matches;
-}
-
-const featuredImage = ()=>{
-  return isDesktop()? './assets/backgrounds/desktop_freatured.png':'./assets/backgrounds/details.jpeg';
-}
-
 const objects = [
   {
-      name: 'Multi-Post Stories Gain+Glory',
-      description : `
+    name: 'Multi-Post Stories Gain+Glory',
+    description: `
   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-      image: './assets/place_holders/plaholder.png',
-      featuredImage: './assets/backgrounds/details.jpeg',
-      desktopFeatured: './assets/backgrounds/desktop_freatured.png',
-      technologies: ['css','html','bootstrap','ruby'],
-      desktopTechnologies : ['CodeKit','Github','Javascript','Terminal','CodePen'],
-      linkToLiveVersion : 'https://matovu-farid.github.io/My-Portfolio',
-      linkToSource: 'https://github.com/matovu-farid/My-Portfolio'
+    image: './assets/place_holders/plaholder.png',
+    featuredImage: './assets/backgrounds/details.jpeg',
+    desktopFeatured: './assets/backgrounds/desktop_freatured.png',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    desktopTechnologies: ['CodeKit', 'Github', 'Javascript', 'Terminal', 'CodePen'],
+    linkToLiveVersion: 'https://matovu-farid.github.io/My-Portfolio',
+    linkToSource: 'https://github.com/matovu-farid/My-Portfolio',
   },
   {
     name: 'Multi-Post Stories Gain+Glory',
-    description : `
+    description: `
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
     image: './assets/place_holders/plaholder.png',
     featuredImage: './assets/backgrounds/details.jpeg',
     desktopFeatured: './assets/backgrounds/desktop_freatured.png',
-    technologies: ['css','html','bootstrap','ruby'],
-    desktopTechnologies : ['CodeKit','Github','Javascript','Terminal','CodePen'],
-    linkToLiveVersion : 'https://matovu-farid.github.io/My-Portfolio',
-    linkToSource: 'https://github.com/matovu-farid/My-Portfolio'
-},
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description : `
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    desktopTechnologies: ['CodeKit', 'Github', 'Javascript', 'Terminal', 'CodePen'],
+    linkToLiveVersion: 'https://matovu-farid.github.io/My-Portfolio',
+    linkToSource: 'https://github.com/matovu-farid/My-Portfolio',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: `
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-  image: './assets/place_holders/plaholder.png',
-  featuredImage: './assets/backgrounds/details.jpeg',
-  desktopFeatured: './assets/backgrounds/desktop_freatured.png',
-  technologies: ['css','html','bootstrap','ruby'],
-  desktopTechnologies : ['CodeKit','Github','Javascript','Terminal','CodePen'],
-  linkToLiveVersion : 'https://matovu-farid.github.io/My-Portfolio',
-  linkToSource: 'https://github.com/matovu-farid/My-Portfolio'
-},
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description : `
+    image: './assets/place_holders/plaholder.png',
+    featuredImage: './assets/backgrounds/details.jpeg',
+    desktopFeatured: './assets/backgrounds/desktop_freatured.png',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    desktopTechnologies: ['CodeKit', 'Github', 'Javascript', 'Terminal', 'CodePen'],
+    linkToLiveVersion: 'https://matovu-farid.github.io/My-Portfolio',
+    linkToSource: 'https://github.com/matovu-farid/My-Portfolio',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: `
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-  image: './assets/place_holders/plaholder.png',
-  featuredImage: './assets/backgrounds/details.jpeg',
-  desktopFeatured: './assets/backgrounds/desktop_freatured.png',
-  technologies: ['css','html','bootstrap','ruby'],
-  desktopTechnologies : ['CodeKit','Github','Javascript','Terminal','CodePen'],
-  linkToLiveVersion : 'https://matovu-farid.github.io/My-Portfolio',
-  linkToSource: 'https://github.com/matovu-farid/My-Portfolio'
-},
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description : `
+    image: './assets/place_holders/plaholder.png',
+    featuredImage: './assets/backgrounds/details.jpeg',
+    desktopFeatured: './assets/backgrounds/desktop_freatured.png',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    desktopTechnologies: ['CodeKit', 'Github', 'Javascript', 'Terminal', 'CodePen'],
+    linkToLiveVersion: 'https://matovu-farid.github.io/My-Portfolio',
+    linkToSource: 'https://github.com/matovu-farid/My-Portfolio',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: `
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-  image: './assets/place_holders/plaholder.png',
-  featuredImage: './assets/backgrounds/details.jpeg',
-  desktopFeatured: './assets/backgrounds/desktop_freatured.png',
-  technologies: ['css','html','bootstrap','ruby'],
-  desktopTechnologies : ['CodeKit','Github','Javascript','Terminal','CodePen'],
-  linkToLiveVersion : 'https://matovu-farid.github.io/My-Portfolio',
-  linkToSource: 'https://github.com/matovu-farid/My-Portfolio'
-},
-{
-  name: 'Multi-Post Stories Gain+Glory',
-  description : `
+    image: './assets/place_holders/plaholder.png',
+    featuredImage: './assets/backgrounds/details.jpeg',
+    desktopFeatured: './assets/backgrounds/desktop_freatured.png',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    desktopTechnologies: ['CodeKit', 'Github', 'Javascript', 'Terminal', 'CodePen'],
+    linkToLiveVersion: 'https://matovu-farid.github.io/My-Portfolio',
+    linkToSource: 'https://github.com/matovu-farid/My-Portfolio',
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description: `
 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-  image: './assets/place_holders/plaholder.png',
-  featuredImage: './assets/backgrounds/details.jpeg',
-  desktopFeatured: './assets/backgrounds/desktop_freatured.png',
-  technologies: ['css','html','bootstrap','ruby'],
-  desktopTechnologies : ['CodeKit','Github','Javascript','Terminal','CodePen'],
-  linkToLiveVersion : 'https://matovu-farid.github.io/My-Portfolio',
-  linkToSource: 'https://github.com/matovu-farid/My-Portfolio'
-},
+    image: './assets/place_holders/plaholder.png',
+    featuredImage: './assets/backgrounds/details.jpeg',
+    desktopFeatured: './assets/backgrounds/desktop_freatured.png',
+    technologies: ['css', 'html', 'bootstrap', 'ruby'],
+    desktopTechnologies: ['CodeKit', 'Github', 'Javascript', 'Terminal', 'CodePen'],
+    linkToLiveVersion: 'https://matovu-farid.github.io/My-Portfolio',
+    linkToSource: 'https://github.com/matovu-farid/My-Portfolio',
+  },
 
-  
-]
+];
 
-
-
-const createListItem = (technology)=>{
-  const listItem = document.createElement('li');
-  listItem.innerHTML = `<p class="tag bg-light-green">${technology}</p>`;
-  return listItem;
-}
-
-const addCloseFunction = (detailsHtml) =>{
-  const close = detailsHtml.querySelector('.close');
-  
-  close.addEventListener('click', () => {
-    detailsHtml.setAttribute('style', 'display:none');
-   
-  });
-}
-
-const addTechnologies = (detailsHtml,{technologies, desktopTechnologies})=> {
-  const tagContainer = detailsHtml.querySelector('.tag-container');
-  const tags = isDesktop()?desktopTechnologies:technologies;
-  tags.forEach(technology => {
-    const listItem = createListItem(technology);
-    tagContainer.appendChild(listItem);
-  });
-}
-
-const showPopup=(detailsHtml) =>{
-  detailsHtml.setAttribute('style', 'display: flex');
-  document.body.appendChild(detailsHtml);
-}
-
-const addLinkToSource = (detailsHtml, linkToSource)=> {
-  const github = detailsHtml.querySelector('.github');
-  github.addEventListener('click', () => window.open(linkToSource));
-}
-
-const addLinkToLive=(detailsHtml, linkToLiveVersion)=> {
-  const seeLive = detailsHtml.querySelector('.see-live');
-  seeLive.addEventListener('click', () => window.open(linkToLiveVersion));
-}
-
-const addButtonFunctionality=(detailsHtml, linkToLiveVersion, linkToSource)=> {
-  addLinkToLive(detailsHtml, linkToLiveVersion);
-  addLinkToSource(detailsHtml, linkToSource);
-  addCloseFunction(detailsHtml);
-}
-
-const makeProjectTechnologies = (projectHtml, technologies)=> {
-  const tagContainer = projectHtml.querySelector('ul');
-  technologies.map((technology) => createListItem(technology)).forEach((node) => {
-    tagContainer.appendChild(node);
-  });
-}
-
-const makeDetails = (object)=>{
-
-  const {linkToLiveVersion,linkToSource,description,featuredImage,desktopFeatured} = object
-  const detailsHtml = createDetailsHtml(featuredImage, description,desktopFeatured);
-  addButtonFunctionality(detailsHtml, linkToLiveVersion, linkToSource);
-  addTechnologies(detailsHtml,object);
-  showPopup(detailsHtml);
-}
-
-const addDetails= (projectHtml, object)=> {
-  const seeResume = projectHtml.querySelector('.button');
-  seeResume.addEventListener('click', () => makeDetails(object));
-}
-
-const makeProject = (object)=>{
-  const {name,image,technologies} = object
-  const projectHtml = createProjectHtml(image, name);      
-       makeProjectTechnologies(projectHtml, technologies);
-       addDetails(projectHtml, object);
-       const myProjects = document.querySelector('.work')
-    myProjects.appendChild(projectHtml)
-}
-
-const createAllProjects = (objects=[])=>{
-  objects.forEach(makeProject);
-}
-createAllProjects(objects);
-
+const isDesktop = () => window.matchMedia('(min-width: 993px)').matches;
 
 function createProjectHtml(image, name) {
   const projectHtml = document.createElement('div');
@@ -198,10 +107,10 @@ function createProjectHtml(image, name) {
   return projectHtml;
 }
 
-function createDetailsHtml(featuredImage, description,desktopFeatured) {
+function createDetailsHtml(featuredImage, description, desktopFeatured) {
   const detailsHtml = document.createElement('section');
-  detailsHtml.classList.add(...['details','blur'])
-  let imageSource = isDesktop()? desktopFeatured:featuredImage;
+  detailsHtml.classList.add(...['details', 'blur']);
+  const imageSource = isDesktop() ? desktopFeatured : featuredImage;
   detailsHtml.innerHTML = `
   <div class="card">
 <i class="fa fa-times close" aria-hidden="true"></i>
@@ -225,3 +134,82 @@ function createDetailsHtml(featuredImage, description,desktopFeatured) {
   return detailsHtml;
 }
 
+const createListItem = (technology) => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `<p class="tag bg-light-green">${technology}</p>`;
+  return listItem;
+};
+
+const addCloseFunction = (detailsHtml) => {
+  const close = detailsHtml.querySelector('.close');
+
+  close.addEventListener('click', () => {
+    detailsHtml.setAttribute('style', 'display:none');
+  });
+};
+
+const addTechnologies = (detailsHtml, { technologies, desktopTechnologies }) => {
+  const tagContainer = detailsHtml.querySelector('.tag-container');
+  const tags = isDesktop() ? desktopTechnologies : technologies;
+  tags.forEach((technology) => {
+    const listItem = createListItem(technology);
+    tagContainer.appendChild(listItem);
+  });
+};
+
+const showPopup = (detailsHtml) => {
+  detailsHtml.setAttribute('style', 'display: flex');
+  document.body.appendChild(detailsHtml);
+};
+
+const addLinkToSource = (detailsHtml, linkToSource) => {
+  const github = detailsHtml.querySelector('.github');
+  github.addEventListener('click', () => window.open(linkToSource));
+};
+
+const addLinkToLive = (detailsHtml, linkToLiveVersion) => {
+  const seeLive = detailsHtml.querySelector('.see-live');
+  seeLive.addEventListener('click', () => window.open(linkToLiveVersion));
+};
+
+const addButtonFunctionality = (detailsHtml, linkToLiveVersion, linkToSource) => {
+  addLinkToLive(detailsHtml, linkToLiveVersion);
+  addLinkToSource(detailsHtml, linkToSource);
+  addCloseFunction(detailsHtml);
+};
+
+const makeProjectTechnologies = (projectHtml, technologies) => {
+  const tagContainer = projectHtml.querySelector('ul');
+  technologies.map((technology) => createListItem(technology)).forEach((node) => {
+    tagContainer.appendChild(node);
+  });
+};
+
+const makeDetails = (object) => {
+  const {
+    linkToLiveVersion, linkToSource, description, featuredImage, desktopFeatured,
+  } = object;
+  const detailsHtml = createDetailsHtml(featuredImage, description, desktopFeatured);
+  addButtonFunctionality(detailsHtml, linkToLiveVersion, linkToSource);
+  addTechnologies(detailsHtml, object);
+  showPopup(detailsHtml);
+};
+
+const addDetails = (projectHtml, object) => {
+  const seeResume = projectHtml.querySelector('.button');
+  seeResume.addEventListener('click', () => makeDetails(object));
+};
+
+const makeProject = (object) => {
+  const { name, image, technologies } = object;
+  const projectHtml = createProjectHtml(image, name);
+  makeProjectTechnologies(projectHtml, technologies);
+  addDetails(projectHtml, object);
+  const myProjects = document.querySelector('.work');
+  myProjects.appendChild(projectHtml);
+};
+
+const createAllProjects = (objects = []) => {
+  objects.forEach(makeProject);
+};
+createAllProjects(objects);
