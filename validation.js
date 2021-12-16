baa476593750dcb83a3ba6932a45e997333af2ce
Farid <matovu90@gmail.com>
2021-12-16 08:38:30 +0300

    Fix: unused variables

--- validation.js
+++ validation.js
@@ -1,23 +1,17 @@
 const email = document.querySelector('#email');
-const submit = document.querySelector('#submit');
-const form = document.querySelector('#contact-form')
+const form = document.querySelector('#contact-form');
 const error = document.querySelector('#error');
-const check = (value)=>console.log(value)
 
-const checkName = ()=>{
-    
-       if(email.validity.patternMismatch){
-           error.textContent = 'The content of the email field has to be in lower case'
-       }else {
-           error.textContent = ''
-       }
-       return email.validity.patternMismatch;
-    
-}
+const checkName = () => {
+  if (email.validity.patternMismatch) {
+    error.textContent = 'The content of the email field has to be in lower case';
+  } else {
+    error.textContent = '';
+  }
+  return email.validity.patternMismatch;
+};
 
-
-form.addEventListener('submit',(event)=>{
-let isNameInValid = checkName();
-if(isNameInValid) event.preventDefault()
-
-})
+form.addEventListener('submit', (event) => {
+  const isNameInValid = checkName();
+  if (isNameInValid) event.preventDefault();
+});
